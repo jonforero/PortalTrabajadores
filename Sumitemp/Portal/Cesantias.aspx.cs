da0f94718bd9d78c66fd0673aa168de79753eb74
m3m0X <guile007@gmail.com>
2016-03-06 10:48:50 -0500

    Se sube el proyecto de Sumitemp y Sumiservis

---
+++ Sumitemp/Portal/Cesantias.aspx.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace PortalTrabajadores.Portal
+{
+    public partial class Estadocesantias : System.Web.UI.Page
+    {
+        string Cn = ConfigurationManager.ConnectionStrings["trabajadoresConnectionString"].ConnectionString.ToString();
+        string ruta = ConfigurationManager.AppSettings["RepositorioPDF"].ToString();
+
+        #region Definicion de los Metodos de la Clase
+
+        #region Metodo Page Load
+        /* ****************************************************************************/
+        /* Metodo que se ejecuta al momento de la carga de la Pagina Maestra
+        /* ****************************************************************************/
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["usuario"] == null)
+            {
+                //Redirecciona a la pagina de login en caso de que el usuario no se halla autenticado
+                Response.Redirect("~/Login.aspx");
+            }
+            else
+            {
+                SqlDataSource1.SelectCommand = "SELECT idCesantia, anio_cesantia, Mes_Cesantia, ruta_archivo_cesantia as ruta,nombre_archivo_cesantia as nombre FROM trabajadores.cesantias where Empleados_Id_Empleado =" + Session["usuario"].ToString() + " and (Empleados_idEmpresa = 'ST' or Empleados_idEmpresa = 'SB');";
+                if (!IsPostBack)
+                {
+                    CnMysql Conexion = new CnMysql(Cn);
+                    try
+                    {
+                        MySqlCommand scSqlCommand = new MySqlCommand("SELECT descripcion FROM basica_trabajador.Options_Menu WHERE url = 'Cesantias.aspx' AND idEmpresa = 'ST'", Conexion.ObtenerCnMysql());
+                        MySqlDataAdapter sdaSqlDataAdapter = new MySqlDataAdapter(scSqlCommand);
+                        DataSet dsDataSet = new DataSet();
+                        DataTable dtDataTable = null;
+                        Conexion.AbrirCnMysql();
+                        sdaSqlDataAdapter.Fill(dsDataSet);
+                        dtDataTable = dsDataSet.Tables[0];
+                        if (dtDataTable != null && dtDataTable.Rows.Count > 0)
+                        {
+
+                            this.lblTitulo.Text = dtDataTable.Rows[0].ItemArray[0].ToString();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MensajeError("Ha ocurrido el siguiente error: " + ex.Message + " _Metodo: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+                    }
+                    finally
+                    {
+                        Conexion.CerrarCnMysql();
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Metodo MensajeError
+        /* ****************************************************************************/
+        /* Metodo que habilita el label de mensaje de error
+        /* ****************************************************************************/
+        public void MensajeError(string Msj)
+        {
+            ContentPlaceHolder cPlaceHolder;
+            cPlaceHolder = (ContentPlaceHolder)Master.FindControl("Errores");
+            if (cPlaceHolder != null)
+            {
+                Label lblMessage = (Label)cPlaceHolder.FindControl("LblMsj") as Label;
+                if (lblMessage != null)
+                {
+                    lblMessage.Text = Msj;
+                    lblMessage.Visible = true;
+                }
+            }
+        }
+        #endregion
+
+        #region Metodo GridView1_RowCommand
+        /* ***********************************************************************************/
+        /* Evento que se ejecuta al momento de dar clic sobre los elementos IMG del GridView
+        /* ***********************************************************************************/
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName == "PDF")
+                {
+                    int index = Convert.ToInt32(e.CommandArgument);
+                    string filepath = "";
+                    string filename2 = "";
+                    DataView dvSql = (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+
+                    //Recorre el SqlDataSource
+                    foreach (DataRowView drvSql in dvSql)
+                    {
+                        if (drvSql["idCesantia"].ToString() == index.ToString())
+                        {
+                            filepath = drvSql["ruta"].ToString();
+                            filename2 = drvSql["nombre"].ToString();
+                            break;
+                        }
+                    }
+
+                    //Valida la Existencia de la ruta Origen para poder descargar el PDF los Archivos
+                    string filename = System.IO.Path.Combine(ruta,filepath, filename2);
+                    if (System.IO.File.Exists(filename))
+                    {
+                        //Response.Write(string.Format("<script>window.open('{0}','_blank');</script>", System.IO.Path.Combine(filepath, filename2)));
+                        Response.Clear();
+                        //Response.ContentType = "application/octet-stream";
+                        //Response.AddHeader("Content-Disposition", "attachment; filename=" + filename2);
+                        //Response.WriteFile(filename);
+                        Response.Buffer = true;
+                        Response.ContentType = "application/pdf";
+                        Response.AddHeader("Content-Disposition", "attachment; filename=" + filename2);
+                        Response.WriteFile(filename);
+                        Response.Flush();
+                        Response.Close();
+                    }
+                    else
+                    {
+                        MensajeError("Aun no se encuentra disponible el certificado.");
+                    }
+                }
+            }
+            catch (Exception E)
+            {
+                MensajeError("Ha ocurrido el siguiente error: " + E.Message + " _Metodo: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Sumitemp/obj/Release/Package/PackageTmp/Portal/Nomina.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Portal/PaginaMaestra.Master" AutoEventWireup="true" CodeBehind="Nomina.aspx.cs" Inherits="PortalTrabajadores.Portal.Nomina" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="ContainerTitulo" runat="server">
+    <asp:UpdatePanel ID="UpdatePanel2" runat="server">
+        <ContentTemplate>
+            <asp:Label ID="lblTitulo" runat="server" />
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="Container" runat="server">
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1" OnRowCommand="GridView1_RowCommand">
+                <AlternatingRowStyle CssClass="ColorOscuro" />
+                <Columns>
+                    <asp:BoundField DataField="temporal" HeaderText="Empresa" SortExpression="temporal" />
+                    <asp:BoundField DataField="DescripcionCargo" HeaderText="Cargo" SortExpression="DescripcionCargo" />
+                    <asp:BoundField DataField="Numero_Nomina" HeaderText="Número de Nómina" SortExpression="Numero_Nomina" />
+                    <asp:BoundField DataField="Descripcion" HeaderText="Tipo de pago" SortExpression="Descripcion" />
+                    <asp:BoundField DataField="Fecha_Generacion_Nomina" HeaderText="Fecha Nómina" SortExpression="Fecha_Generacion_Nomina" />
+                    <asp:BoundField DataField="Empleados_idContrato" HeaderText="ID Contrato" SortExpression="Empleados_idContrato" Visible="false" />
+                    <asp:TemplateField HeaderText="Generar" ItemStyle-HorizontalAlign="Center">
+                        <ItemTemplate>
+                            <asp:ImageButton ID="Imgpdf" runat="server" ImageUrl="~/Img/printButton.png" CommandArgument='<%#Eval("Fecha_Generacion_Nomina") +";"+ Eval("Numero_Nomina") +";"+ Eval("Empleados_idContrato") %>' CommandName="Comprobante" />
+                        </ItemTemplate>
+                        <ItemStyle HorizontalAlign="Center" />
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:trabajadoresConnectionString %>" ProviderName="<%$ ConnectionStrings:trabajadoresConnectionString.ProviderName %>" SelectCommand="trabajadores.sp_numcomprobantesEmp" SelectCommandType="StoredProcedure">
+                <SelectParameters>
+                    <asp:SessionParameter Name="idEmpleado" SessionField="usuario" Type="String" />
+                    <asp:SessionParameter Name="Etemporal"  Type="String" DefaultValue="ST" />
+                </SelectParameters>
+            </asp:SqlDataSource>
+        </ContentTemplate>
+        <Triggers>
+            <asp:PostBackTrigger ControlID="GridView1"/>
+        </Triggers>
+    </asp:UpdatePanel>
+</asp:Content>
